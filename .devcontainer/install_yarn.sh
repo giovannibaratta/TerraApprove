bede9208aee373330d0dbcb37be42805052168fe
Giovanni Baratta <bargiovi@hotmail.it>
2023-07-22 14:52:18 +0000

    AB#9 Add yarn 3

---
+++ .devcontainer/install_yarn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# Enable yarn 3 (see https://yarnpkg.com/getting-started/install)
+
+"$NVM_BIN/corepack" enable
+"$NVM_BIN/corepack" prepare yarn@stable --activate
