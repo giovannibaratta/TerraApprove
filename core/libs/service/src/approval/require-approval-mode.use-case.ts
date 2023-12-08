635e8030c659a9a2ec3f6bf32c2a497bd4845366
Giovanni Baratta <bargiovi@hotmail.it>
2023-12-08 10:30:26 +0000

    AB#93 refactor: extract logic used to check if approval is required for require approval mode

--- core/libs/service/src/approval/require-approval-mode.use-case.ts
+++ core/libs/service/src/approval/require-approval-mode.use-case.ts
@@ -20,21 +20,8 @@ export class RequireApprovalModeUseCase {
     const {diffsEntityPairs, configuration} = data
 
     // From all the diffs, keep only the ones that requires approval
-    const resourcesThatRequiredApproval = diffsEntityPairs.filter(
-      pair =>
-        // Check if the provider type is in the list of types that always require approval
-        configuration.global.requireApprovalItems
-          ?.map(it => it.providerType)
-          .includes(pair[0].providerType) ||
-        // Verify first if one of the action to achieve the diffType is in the list of actions that
-        // always require approval. If this is the case the resource requires approval.
-        this.doesContainActionThatAlwaysRequireApproval(
-          configuration,
-          pair[0].diffType
-        ) ||
-        // If no match is found check is there is a specific decorator associated to the resource.
-        (pair[1].decorator.type === "manual_approval" &&
-          isDiffActionIncludedInEntityDecorator(pair[1].decorator, pair[0]))
+    const resourcesThatRequiredApproval = diffsEntityPairs.filter(pair =>
+      this.doesDiffRequireApproval(configuration, pair[0], pair[1])
     )
 
     Logger.log(
@@ -45,6 +32,28 @@ export class RequireApprovalModeUseCase {
     )
 
     return resourcesThatRequiredApproval.length > 0
+  }
+
+  private doesDiffRequireApproval(
+    configuration: Configuration,
+    diff: TerraformDiff,
+    entity: TerraformEntity
+  ): boolean {
+    return (
+      // Check if the provider type is in the list of types that always require approval
+      configuration.global.requireApprovalItems
+        ?.map(it => it.providerType)
+        .includes(diff.providerType) ||
+      // Verify first if one of the action to achieve the diffType is in the list of actions that
+      // always require approval. If this is the case the resource requires approval.
+      this.doesContainActionThatAlwaysRequireApproval(
+        configuration,
+        diff.diffType
+      ) ||
+      // If no match is found check is there is a specific decorator associated to the resource.
+      (entity.decorator.type === "manual_approval" &&
+        isDiffActionIncludedInEntityDecorator(entity.decorator, diff))
+    )
   }
 
   private doesContainActionThatAlwaysRequireApproval(
