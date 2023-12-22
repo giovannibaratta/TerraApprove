684c5af22c89d6460d85a3c40d449d71486f0d31
Giovanni Baratta <bargiovi@hotmail.it>
2023-12-22 16:16:06 +0000

    AB#101 feat: Define Dockerfile that run TerraApprove executable

---
+++ docker/validate-plan.sh
@@ -0,0 +1,37 @@
+# Validate input variables
+: ${INPUT_TF_CODE_DIR?"The environment variable 'INPUT_TF_CODE_DIR' is not set."}
+: ${INPUT_TF_PLAN_JSON_FILE?"The environment variable 'INPUT_TF_PLAN_JSON_FILE' is not set."}
+
+INPUT_MODE=${INPUT_MODE:-"standard"}
+
+if [ "$INPUT_MODE" == "standard" ]; then
+  flag_mode="--standard"
+elif [ "$INPUT_MODE" == "safe_to_apply" ]; then
+  flag_mode="--reverse"
+else
+  echo "Error: The environment variable 'INPUT_MODE' has an invalid value."
+  exit 1
+fi
+
+# Setup output
+# If we are running in a GitHub Action, write to the GitHub Action output
+OUTPUT="${GITHUB_OUTPUT:-/dev/stdout}"
+
+# Execute TerraApprove
+/app/terraapprove "${INPUT_TF_CODE_DIR}" "${INPUT_TF_PLAN_JSON_FILE}" ${flag_mode} > /dev/null
+
+# Evaluate the exit code
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+  # No approval is required
+  echo "false" >> $OUTPUT
+  exit 0
+elif [ $exit_code -eq 1 ]; then
+  # Approval is required
+  echo "true" >> $OUTPUT
+  exit 0
+else
+  # TerraApprove exited with an error. Propagate the error.
+  exit 1
+fi
