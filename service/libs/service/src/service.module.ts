bfcce4e66044d2779ac21a1af29cc82bad81da99
Giovanni Baratta <bargiovi@hotmail.it>
2024-02-11 10:37:07 +0000

    chore: remove hello world setup from service

--- service/libs/service/src/service.module.ts
+++ service/libs/service/src/service.module.ts
@@ -1,11 +1,10 @@
 import {Module} from "@nestjs/common"
-import {AppService} from "./app.service"
 import {SourceCodeService} from "./source-code.service"
 import {ExternalModule} from "@libs/external/external.module"
 
 @Module({
   imports: [ExternalModule],
-  providers: [AppService, SourceCodeService],
-  exports: [AppService, SourceCodeService]
+  providers: [SourceCodeService],
+  exports: [SourceCodeService]
 })
 export class ServiceModule {}
--- service/main/src/app.module.ts
+++ service/main/src/app.module.ts
@@ -1,11 +1,10 @@
 import {Module} from "@nestjs/common"
-import {AppController} from "./app.controller"
 import {ServiceModule} from "@libs/service/service.module"
 import {SourceCodeController} from "./controller/source-code.controller"
 
 @Module({
   imports: [ServiceModule],
-  controllers: [AppController, SourceCodeController],
+  controllers: [SourceCodeController],
   providers: []
 })
 export class AppModule {}
