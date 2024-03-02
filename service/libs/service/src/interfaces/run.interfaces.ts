d21886bdd6a403ad413af43dae34267080fa2d4d
Giovanni Baratta <bargiovi@hotmail.it>
2024-03-02 17:02:13 +0000

    AB#143 feat: add RunEventPublisher interface

--- service/libs/service/src/interfaces/run.interfaces.ts
+++ service/libs/service/src/interfaces/run.interfaces.ts
@@ -5,10 +5,22 @@ export interface RunRepository {
   createRun(request: CreateRun): TaskEither<"unknown_run_state", BaseRun>
 }
 
+export interface RunEventPublisher {
+  publishRunState(runState: RunState): TaskEither<never, void>
+}
+
 export interface CreateRun {
   baseRun: BaseRun
   sourceCodeId: string
   planId: string
 }
 
+export interface RunState {
+  state: BaseRun["state"]
+  id: string
+  revision: bigint
+  updatedAt: Date
+}
+
 export const RUN_REPOSITORY_TOKEN = "RUN_REPOSITORY_TOKEN"
+export const RUN_EVENT_PUBLISHER_TOKEN = "RUN_EVENT_PUBLISHER_TOKEN"
