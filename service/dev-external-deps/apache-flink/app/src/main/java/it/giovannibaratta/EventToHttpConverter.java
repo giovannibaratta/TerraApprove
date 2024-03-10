6820cbcc75f4b3d4670513d14e909502d26cb95c
Giovanni Baratta <bargiovi@hotmail.it>
2024-03-10 17:05:48 +0000

    AB#154 feat: add http request forwaring in EventToHttpConverter job

--- service/dev-external-deps/apache-flink/app/src/main/java/it/giovannibaratta/EventToHttpConverter.java
+++ service/dev-external-deps/apache-flink/app/src/main/java/it/giovannibaratta/EventToHttpConverter.java
@@ -1,10 +1,14 @@
 package it.giovannibaratta;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -15,21 +19,41 @@ public class EventToHttpConverter {
     ParameterTool parameters = ParameterTool.fromArgs(args);
 
     String kafkaBootstrapServers = parameters.getRequired("kafkaBootstrapServers");
-    String kafkaTopic = parameters.getRequired("kafkaTopic");
+    String[] kafkaTopics = parameters.getRequired("kafkaTopics").split(",");
+
+    if (kafkaTopics.length < 1) {
+      throw new IllegalArgumentException("At least one Kafka topic must be provided");
+    }
+
+    Map<String, String> kafkaTopicDestination = new HashMap<String, String>();
+
+    for (String topic : kafkaTopics) {
+      String destination = parameters.getRequired(topic);
+
+      if(destination == null || destination.isEmpty() {
+        throw new IllegalArgumentException("Destination for topic " + topic + " must be provided");
+      }
+
+      kafkaTopicDestination.put(topic, destination);
+    }
+
+    AsyncHttpClient httpClient = new AsyncHttpClient(kafkaTopicDestination);
 
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-    KafkaSource<String> source = KafkaSource.<String>builder()
+    KafkaSource<KafkaEvent> source = KafkaSource.<KafkaEvent>builder()
         .setBootstrapServers(kafkaBootstrapServers)
-        .setTopics(kafkaTopic)
-        .setGroupId("flink")
-        .setStartingOffsets(OffsetsInitializer.earliest())
-        .setValueOnlyDeserializer(new SimpleStringSchema())
+        .setTopics(kafkaTopics)
+        .setStartingOffsets(OffsetsInitializer.latest())
+        .setDeserializer(new KafkaDeserializer())
         .build();
 
-    DataStream<String> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+    DataStream<KafkaEvent> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-    // Print each event to the console
+    // The provided transformation does not retry in case of failed requests
+    AsyncDataStream.unorderedWait(stream, httpClient, 1000, TimeUnit.MILLISECONDS, 100);
+
+    // Print messages for debugging
     stream.print();
 
     env.execute("Kafka events to HTTP requests");
