398c3c3b16b810ac1407053b859740ea332eeec3
Giovanni Baratta <bargiovi@hotmail.it>
2024-03-17 20:58:21 +0000

    AB#162 feat: add interface for ValidationControllerEventPublisher

--- service/libs/service/src/interfaces/validation-controller.interfaces.ts
+++ service/libs/service/src/interfaces/validation-controller.interfaces.ts
@@ -10,6 +10,15 @@ export interface ValidationControllerRepository {
   >
 }
 
+export interface ValidationControllerEventPublisher {
+  publishValidationSourceCodeState(
+    validationSourceCodeState: ValidationSourceCodeState
+  ): TaskEither<never, void>
+  publishValidationPlanState(
+    validationPlanState: ValidationPlanState
+  ): TaskEither<never, void>
+}
+
 export type CreateValidationControllerItemError =
   | "run_not_found"
   | "validation_controller_already_exist_for_run"
@@ -21,5 +30,22 @@ export interface CreateValidationController {
   runId: string
 }
 
+export interface ValidationSourceCodeState {
+  state: BaseValidationControllerItem["sourceCodeState"]
+  id: string
+  revision: bigint
+  updatedAt: Date
+}
+
+export interface ValidationPlanState {
+  state: BaseValidationControllerItem["planState"]
+  id: string
+  revision: bigint
+  updatedAt: Date
+}
+
 export const VALIDATION_CONTROLLER_REPOSITORY_TOKEN =
   "VALIDATION_CONTROLLER_REPOSITORY_TOKEN"
+
+export const VALIDATION_CONTROLLER_EVENT_PUBLISHER_TOKEN =
+  "VALIDATION_CONTROLLER_EVENT_PUBLISHER_TOKEN"
